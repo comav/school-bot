e3be2c79fef3c69a004c012d010d9958d3c8c0ef
comav <tsyomav@gmail.com>
2021-03-08 00:36:33 +0200

    Added language buttons

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-const { Telegraf } = require('telegraf');
+const { Telegraf, Markup } = require('telegraf');
 const dotenv = require('dotenv');
+const { Keyboard, Key } = require('telegram-keyboard');
 
 dotenv.config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) => {ctx.reply('Hi, please choose your preffered language')});
+let language = undefined;
+
+bot.start((ctx) => {
+    let keyboard = Keyboard.inline([
+        Key.callback('🇺🇦', language = 'ukrainian'),
+        Key.callback('🇬🇧', language = 'english')
+    ])
+    ctx.reply('Choose your language', keyboard);
+});
+
+bot.command('mylang', (ctx) => {
+    ctx.reply(language);
+})
 
 bot.launch();
+
+console.log('Bot is running...');
+console.log(language);
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
